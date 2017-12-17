17124d7d7e07cf67a040a2ea2c6d7e48ee68598e
Crotex <crotexrl@gmail.com>
2017-12-17 18:58:53 +0100

    Remove comments

--- RocketLeagueStatChecker/Main_Form.cs
+++ RocketLeagueStatChecker/Main_Form.cs
@@ -61,98 +61,46 @@ namespace RocketLeagueStatChecker
             }
         }
 
-        //Check and set the selected Season
+        //Get and set the selected Season
         private void setSeason()
         {
-            //Get selected season
             string item = seasonBox.SelectedItem.ToString();
 
-            //Season set to 1
             if (item == "Season 1")
             {
                 season_set = true;
                 s = RlsSeason.One;
             }
 
-            //Season set to 2
             if (item == "Season 2")
             {
                 season_set = true;
                 s = RlsSeason.Two;
             }
 
-            //Season set to 3
             if (item == "Season 3")
             {
                 season_set = true;
                 s = RlsSeason.Three;
             }
 
-            //Season set to 4
             if (item == "Season 4")
             {
                 season_set = true;
                 s = RlsSeason.Four;
             }
 
-            //Season set to 5
             if (item == "Season 5")
             {
                 season_set = true;
                 s = RlsSeason.Five;
             }
 
-            //Season set to 6
             if (item == "Season 6")
             {
                 season_set = true;
                 s = RlsSeason.Six;
             }
         }
-
-        /*Check and set the selected platform
-        private void setPlatform()
-        {
-            //Platform = Steam
-            if (Player_Name_Platform.platform == 1)
-            {
-                plat = RlsPlatform.Steam;
-                platform_set = true;
-            }
-
-            //Platform = PS4
-            if (Player_Name_Platform.platform == 2)
-            {
-                plat = RlsPlatform.Ps4;
-                platform_set = true;
-            }
-
-            //Platform = XBOX
-            if (Player_Name_Platform.platform == 3)
-            {
-                plat = RlsPlatform.Xbox;
-                platform_set = true;
-            }
-        }*/
-
-        /*Check the ranks
-        public static async Task Run()
-        {
-            var apiKey = "IQB8PMF8N605UKX7K698FTWWV99J2G9M";
-            var client = new RLSClient(apiKey);
-
-            var player = await client.GetPlayerAsync(plat, Player_Name_Platform.name);
-
-            var playerSeason = player.RankedSeasons.FirstOrDefault(x => x.Key == s);
-
-            if (playerSeason.Value != null)
-            {
-                MessageBox.Show("Player: " + player.DisplayName);
-                foreach (var playerRank in playerSeason.Value)
-                {
-                    MessageBox.Show(playerRank.Key + ": " + playerRank.Value.RankPoints + " rating");
-                }
-            }
-        }*/
     }
 }
